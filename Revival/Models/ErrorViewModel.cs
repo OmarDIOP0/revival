b1fab78bdc2067e2f9abd1209489d104e60c12ef
OmarDIOP0 <odiop6170@gmail.com>
2026-08-12 16:43:53 +0000

    initial comit

---
+++ Revival/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Revival.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
